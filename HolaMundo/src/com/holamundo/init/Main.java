f4b44434a0c480db52c602593e342b279e71dec3
xandro2021 <xandrocastro2021@gmail.com>
2025-03-19 09:01:46 -0600

    cambios

--- HolaMundo/src/com/holamundo/init/Main.java
+++ HolaMundo/src/com/holamundo/init/Main.java
@@ -1,5 +1,9 @@
 package com.holamundo.init;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 import com.holamundo.mvc.controllers.ControllerPersonas;
 
 public class Main {
@@ -7,6 +11,13 @@ public class Main {
   public static void main(String[] args) {
 
     new ControllerPersonas().init();
+
+    int misdatos = Integer.parseInt(JOptionPane.showInputDialog("Ingresa entero"));
+    System.out.println(misdatos);
+    misdatos++;
+    System.out.println(misdatos);
+    misdatos++;
+    System.out.println(misdatos);
 
   }
 
